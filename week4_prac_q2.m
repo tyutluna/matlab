08a05e23ee020e86bbf47bc16e185342a4598869
tyutluna <88483363+tyutluna@users.noreply.github.com>
2021-08-20 20:11:55 +0930

    Add files via upload

---
+++ week4_prac_q2.m
@@ -0,0 +1,26 @@
+% make a 2d array of values.
+imageData=[]; % set up an empty array
+imSize=100;
+numRows=imSize;
+numCols=imSize;
+
+%populate using a nested loop
+for row=[1:numRows]
+    imageRow=[]; %empty vector for row
+    for col=[1:numCols]
+        pixelVal=1-row/imSize; % make a pixel val in 0..1
+        imageRow=[imageRow pixelVal];  %add value for  pixel
+    end
+    imageData=[imageData; imageRow]; % add row of pixels
+end
+
+
+% display as an image
+imshow(imageData);
+
+% save as an image
+imwrite(imageData,'test.png');
+
+% load an image
+newImageData=imread('test.png');
+whos newImageData;
